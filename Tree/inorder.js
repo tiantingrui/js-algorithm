0f28869c78d5a5e45c5d9dabc99ab8ffcf4e504d
terry <976912095@qq.com>
2021-07-25 18:46:44 +0800

    feat(Tree): 二叉树先中后序遍历非递归版本实现

--- Tree/inorder.js
+++ Tree/inorder.js
@@ -1,13 +1,28 @@
 // 中序遍历
-const bt = require(
-  './bt'
-)
+const bt = require("./bt");
 
+// const inorder = (root) => {
+//   if (!root) return
+//   inorder(root.left)
+//   console.log(root.val)
+//   inorder(root.right)
+// }
+
+// 用栈来实现 二叉树的中序遍历
 const inorder = (root) => {
-  if (!root) return
-  inorder(root.left)
-  console.log(root.val)
-  inorder(root.right)
-}
+  if (!root) return;
+  const stack = [];
+  let p = root;
+  while (stack.length || p) {
+    // 将所有左子树推送到栈中
+    while (p) {
+      stack.push(p);
+      p = p.left;
+    }
+    const n = stack.pop();
+    console.log(n.val);
+    p = n.right;
+  }
+};
 
-inorder(bt)
+inorder(bt);
--- Tree/preorder.js
+++ Tree/preorder.js
@@ -1,14 +1,30 @@
 // 先序遍历
 const bt = require('./bt')
 
+// 递归版本
+// const preorder = (root) => {
+//   if (!root) return 
+//   // 1. 访问根节点
+//   console.log(root.val)
+//   // 2. 对根节点的左子树进行先序遍历（递归）
+//   preorder(root.left)
+//   // 3. 对根节点的右子树进行先序遍历（递归）
+//   preorder(root.right)
+// }
+
+// 非递归版本，利用堆栈实现
 const preorder = (root) => {
   if (!root) return 
-  // 1. 访问根节点
-  console.log(root.val)
-  // 2. 对根节点的左子树进行先序遍历（递归）
-  preorder(root.left)
-  // 3. 对根节点的右子树进行先序遍历（递归）
-  preorder(root.right)
+  const stack = [root]
+  while (stack.length) {
+    const n = stack.pop()
+    // 访问根节点的值
+    console.log(n.val)
+    // 这里注意先推送 右子树，由于栈是后进先出
+    if (n.right) stack.push(n.right)
+    if (n.left) stack.push(n.left)
+  }
+
 }
 
 preorder(bt)
